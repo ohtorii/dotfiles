b2fdf608c37262da9c3a2ac7bf10262911eb527e
ohtorii <ikeuchi.h9@gmail.com>
2018-09-01 10:19:04 +0900

    refactoring

--- setup_windows.bat
+++ setup_windows.bat
@@ -5,9 +5,14 @@ REM
 setlocal
 set HOME=%HOMEDRIVE%%HOMEPATH%
 
-echo agignore
-mklink /H "%HOME%\.agignore" "%~dp0.agignore"
-echo .vimrc
-mklink /H "%HOME%\_vimrc" "%~dp0.vimrc"
-echo .gvimrc
-mklink /H "%HOME%\_gvimrc" "%~dp0.gvimrc"
+call :proc .agignore .agignore
+call :proc _vimrc .vimrc
+call :proc _gvimrc .gvimrc
+exit /b 0
+
+
+:proc
+echo %1
+del /F "%HOME%\%1"
+mklink /H "%HOME%\%1" "%~dp0%2"
+exit /b 0
